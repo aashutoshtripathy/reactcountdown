4f8a73fba39f51371e3ef5c072a2cdd90ed7dae3
Ashutosh <ashutoshkmr419@gmail.com>
2024-01-20 12:00:57 +0530

    modify

---
+++ react_project03/src/components/ProgressBar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { useState,useEffect } from 'react'
+
+const ProgressBar = ({timer}) => {
+    const [remainingTime, setRemainingTime] = useState(timer)
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setRemainingTime((prevTime) => prevTime - 10)
+        }, 10)
+        return() => {
+            clearInterval(interval);
+        }
+    }, [])
+  return (
+    <div>
+        <progress value={remainingTime} max={timer} />
+    </div>
+  )
+}
+
+export default ProgressBar
