20ba642ab7487ca5b01506f339f4857fd24b1543
Ashutosh <ashutoshkmr419@gmail.com>
2024-01-24 17:29:51 +0530

    modify

---
+++ react_project05/src/components/Answers.js
@@ -0,0 +1,34 @@
+import React, { useRef } from 'react'
+
+const Answers = ({answers, selectedAnswer, answerState, onSelect}) => {
+
+
+    const suffeledAnswers = useRef();
+
+    if (!suffeledAnswers.current) {
+        suffeledAnswers.current = [...answers];
+        suffeledAnswers.current.sort(() => Math.random() - 0.5);
+        }
+
+
+
+  return (
+    <div>
+        <ul id='answers'>
+        {suffeledAnswers.current.map(answer => {
+          const isSelected = selectedAnswer === answer;
+          let cssClass = '';
+          if (answerState === 'Answered' && isSelected) {
+            cssClass = 'selected';
+          }
+          if((answerState==='correct'||answerState==='wrong') && isSelected){
+            cssClass = answerState;
+          }
+          return<li key={answer} className='answer'><button onClick={() => onSelect(answer)} className={cssClass}>{answer}</button></li>
+        } )}
+        </ul>
+    </div>
+  )
+}
+
+export default Answers
