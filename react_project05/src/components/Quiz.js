d55c0c00d477652b69f6671345e2593de130066c
Ashutosh <ashutoshkmr419@gmail.com>
2024-01-25 17:33:39 +0530

    modified

--- react_project05/src/components/Quiz.js
+++ react_project05/src/components/Quiz.js
@@ -2,42 +2,27 @@ import React from 'react'
 import { useState, useCallback } from 'react'
 import QUESTIONS from '../questions'
 import completeQuiz from '../assets/quiz-complete.png'
-import QuestionTimmer from './QuestionTimmer'
-import Answers from './Answers'
 import Question from './Question'
 
 const Quiz = () => {
   // const [activeQuestions, setActiveQuestions] = useState(0)
   // const suffeledAnswers = useRef();
-  const [answerState, setAnswerState] = useState('')
+  // const [answerState, setAnswerState] = useState('')
   const [userAnswer, setUserAnswer] = useState([])
 
 
- const activeQuestionIndex = answerState === '' ? userAnswer.length : userAnswer.length - 1;
+ const activeQuestionIndex = userAnswer.length;
 
   const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
 
   const handleSelectAnswer = useCallback(function handleSelectAnswer(selectedAnswer) {
-    setAnswerState('Answered')
     setUserAnswer((prevAnswer) => {
       return [...prevAnswer, selectedAnswer]
     });
 
 
-    setTimeout(() => {
-      if (selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]) {
-        setAnswerState('correct')
-      }else{
-        setAnswerState('wrong')
-      }
-
-
-      setTimeout(() => {
-        setAnswerState('')
-      }, 2000);
-    }, 1000);
-  },[activeQuestionIndex])
+  },[]);
 
 
  
@@ -59,7 +44,7 @@ const Quiz = () => {
 
   return (
     <div id='quiz'>
-    <Question questionText={QUESTIONS[activeQuestionIndex].text} answers={QUESTIONS[activeQuestionIndex].answers} onSelectAnswer={handleSelectAnswer}/>
+    <Question index={activeQuestionIndex} onSelectAnswer={handleSelectAnswer}   onSkipAnswer={handleSkipAnswer} />
     </div>
   )
 }
