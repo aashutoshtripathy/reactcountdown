c94896923152afdbfe625a9121e9934d66a8c8ed
Ashutosh <ashutoshkmr419@gmail.com>
2024-01-27 16:38:26 +0530

    modified

--- react_project06/src/App.js
+++ react_project06/src/App.js
@@ -21,7 +21,7 @@ function App() {
       <Header />
       <main>
         <ConfigureCounter onSet={handleSetCount}/>
-        <Counter initialCount={chosenCount} />
+        <Counter key={chosenCount} initialCount={chosenCount} />
       </main>
     </>
   );
