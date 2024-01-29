d324a37256789182abf3bc6b689aab85b290f777
Ashutosh <ashutoshkmr419@gmail.com>
2024-01-29 13:55:14 +0530

    modified

---
+++ react_project07/src/components/AvailablePlaces.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react';
+import Places from './Places';
+
+
+export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([])
+
+
+  useEffect(()=>{
+    fetch('http://localhost:30000/places').then((response) => {
+    return response.json()
+  }).then((resData) => {
+    return setAvailablePlaces(resData.places)
+  })
+  }, [])
+
+
+  
+  return (
+    <Places
+      title="Available Places"
+      places={availablePlaces}
+      fallbackText="No places available."
+      onSelectPlace={onSelectPlace}
+    />
+  );
+}
