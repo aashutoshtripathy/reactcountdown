f5c4c44682c76d1f18651d146b67cf37233a004f
Ashutosh <ashutoshkmr419@gmail.com>
2024-02-02 14:21:54 +0530

    modified

--- react_project07/src/components/AvailablePlaces.js
+++ react_project07/src/components/AvailablePlaces.js
@@ -1,35 +1,51 @@
-import { useState, useEffect } from 'react';
-import Places from './Places';
-
+import { useState, useEffect } from "react";
+import Places from "./Places";
+import Error from "./Error";
+import { sortPlacesByDistance } from "../loc";
+import { fetchAvailablePlaces } from "../http";
 
 export default function AvailablePlaces({ onSelectPlace }) {
-  const [isFetching, setIsFetching] = useState(false)
-  const [availablePlaces, setAvailablePlaces] = useState([])
+  const [isFetching, setIsFetching] = useState(false);
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState();
 
-
-  useEffect(()=>{
-
-    async function fetchPlaces () {
+  useEffect(() => {
+    async function fetchPlaces() {
       setIsFetching(true);
-    const response = await fetch('http://localhost:30000/places');
-    const resData = await response.json();
-    setAvailablePlaces(resData.places)
-    setIsFetching(false);
+      try {
+        const places = await fetchAvailablePlaces()
+
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+            places,
+            position.coords.latitude,
+            position.coords.longitude
+          );
+          setAvailablePlaces(sortedPlaces);
+          setIsFetching(false);
+        });
+      } catch (error) {
+        setError({
+          message: error.message || "could not fetch , please try again later.",
+        });
+      }
+      setIsFetching(false);
     }
 
-
     fetchPlaces();
-  }, [])
+  }, []);
 
+  if (error) {
+    return <Error tittle="An error occured" message={error.message} />;
+  }
 
-  
   return (
     <Places
       title="Available Places"
       places={availablePlaces}
       isLoading={isFetching}
       fallbackText="No places available."
-      loadingText = "fetching places data...."
+      loadingText="fetching places data...."
       onSelectPlace={onSelectPlace}
     />
   );
