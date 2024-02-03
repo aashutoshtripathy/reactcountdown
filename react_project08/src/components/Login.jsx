175e93a745537f2f8de253aca4a8dc8cd996c8c9
Ashutosh <ashutoshkmr419@gmail.com>
2024-02-03 10:11:23 +0530

    modified

---
+++ react_project08/src/components/Login.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+export default function Login() {
+
+  const [inpVal, setInpVal] = useState({
+    email: '',
+    password: ''
+  })
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log('Submitted!!');
+    console.log(inpVal);
+    event.target.reset();
+  }
+
+  function handleChange(identifier, value) {
+    setInpVal(prevInpVal => ({
+      ...prevInpVal,
+      [identifier]: value
+    }))
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Login</h2>
+
+      <div className="control-row">
+        <div className="control no-margin">
+          <label htmlFor="email">Email</label>
+          <input id="email" type="email" name="email" value={inpVal.email} onChange={(event) => handleChange('email',event.target.value)}/>
+        </div>
+
+        <div className="control no-margin">
+          <label htmlFor="password">Password</label>
+          <input id="password" type="password" name="password" value={inpVal.password} onChange={(event) => handleChange('password',event.target.value)}/>
+        </div>
+      </div>
+
+      <p className="form-actions">
+        <button className="button button-flat" type="reset">Reset</button>
+        <button className="button">Login</button>
+      </p>
+    </form>
+  );
+}
