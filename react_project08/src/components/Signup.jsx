286354f3e2b197327960234d91b79ffb6b09ea1e
Ashutosh <ashutoshkmr419@gmail.com>
2024-02-03 14:23:12 +0530

    modified

--- react_project08/src/components/Signup.jsx
+++ react_project08/src/components/Signup.jsx
@@ -1,6 +1,11 @@
 export default function Signup() {
   function handleSubmit(event) {
     event.preventDefault();
+
+
+    const fd = new FormData(event.target);
+    const data = Object.fromEntries(fd.entries());
+    console.log(data);
   }
   return (
     <form onSubmit={handleSubmit}>
